7e4610d7689e56c026b4288f8218f5de9b445350
Maciej Korczala <maciej.coders@gmail.com>
2021-04-20 16:12:43 +0200

    commit - prework, part2

--- src/main/java/pl/coderslab/bzmienneioperatory/Main01.java
+++ src/main/java/pl/coderslab/bzmienneioperatory/Main01.java
@@ -2,8 +2,12 @@ package pl.coderslab.bzmienneioperatory;
 
 public class Main01 {
 
-	public static void main(String[] args) {
-
-	}
+    public static void main(String[] args) {
+        int int1 = 45898;
+        char char2 = 'k';
+        long long3 = 45345343343345L;
+        double double4 = 0.9893489;
+        boolean boolean5 = false;
+    }
 
 }
