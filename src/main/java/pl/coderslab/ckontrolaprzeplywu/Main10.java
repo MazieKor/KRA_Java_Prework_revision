07e5e72a10dcf8976052fc23994cfe26096fd1ae
Maciej Korczala <maciej.coders@gmail.com>
2021-04-28 20:10:00 +0200

    arrays exercises, 1.1

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main10.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main10.java
@@ -27,5 +27,27 @@ public class Main10 {
                     System.out.print("");
             }
         }
+
+//        2 rozwiązanie:
+//        int n = 5;
+//        for(int i = 0; i < 2 * n; i++) {
+//            for(int j = 0; j < n; j++) {
+//                if(j <= i && i < n) {
+//                    System.out.print("* ");
+//                }
+//                else if(j > i && i < n) {
+//                    System.out.print(j + 1 + " ");
+//                }
+//                else if(i >= n && 2 * n - j > i ) {
+//                    System.out.print("* ");
+//                }
+//                else {
+//                    System.out.print(j + 1 + " ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
     }
 }
--- src/main/java/pl/coderslab/dtablice/Main01.java
+++ src/main/java/pl/coderslab/dtablice/Main01.java
@@ -3,7 +3,11 @@ package pl.coderslab.dtablice;
 public class Main01 {
 
     public static void main(String[] args) {
-
+        int[] threeElements = new int [3];
+        threeElements[0] = 3;
+        threeElements[1] = 4;
+        threeElements[2] = 5;
+        System.out.println(threeElements[0] + " " + threeElements[1] + " " + threeElements[2]);
     }
 
 }
