4163eb7e1806b78400f69b167978bcbcf93d8704
Maciej Korczala <maciej.coders@gmail.com>
2021-04-28 21:12:25 +0200

    arrays exercises, 1.2

--- src/main/java/pl/coderslab/dtablice/Main06.java
+++ src/main/java/pl/coderslab/dtablice/Main06.java
@@ -3,7 +3,14 @@ package pl.coderslab.dtablice;
 public class Main06 {
 
 	public static void main(String[] args) {
-
+		int[] numbers = {4, 643, 112, 9999, -69};
+		for (int i = numbers.length - 1; i >= 0; i--) {
+			if (i == 0) {
+				System.out.print(numbers[i]);
+			} else {
+				System.out.println(numbers[i]);
+			}
+		}
 	}
 
 }
