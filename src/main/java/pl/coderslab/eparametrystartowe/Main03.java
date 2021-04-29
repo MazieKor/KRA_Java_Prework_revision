f15a93e09f43705468c7312c0f9ebebb592f567f
Maciej Korczala <maciej.coders@gmail.com>
2021-04-29 13:08:53 +0200

    start parameters, 1.1

--- src/main/java/pl/coderslab/eparametrystartowe/Main03.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main03.java
@@ -2,6 +2,12 @@ package pl.coderslab.eparametrystartowe;
 
 public class Main03 {
     public static void main(String[] args) {
-
+        double sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            int argsInt = Integer.parseInt(args[i]);
+            sum += argsInt;
+        }
+        double avg = sum / args.length;
+        System.out.print(avg);
     }
 }
--- src/main/java/pl/coderslab/eparametrystartowe/Main04.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main04.java
@@ -3,7 +3,13 @@ package pl.coderslab.eparametrystartowe;
 public class Main04 {
 
     public static void main(String[] args) {
-
+        if (Integer.parseInt(args[0]) + Integer.parseInt(args[1]) > Integer.parseInt(args[2]) &&
+                Integer.parseInt(args[0]) + Integer.parseInt(args[2]) > Integer.parseInt(args[1]) &&
+                Integer.parseInt(args[1]) + Integer.parseInt(args[2]) > Integer.parseInt(args[0])) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
     }
 
 }
